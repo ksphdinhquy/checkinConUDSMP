d744f0ebbfb7e251fff7622a6e2734239a6594ea
ksphdinhquy <151590399+ksphdinhquy@users.noreply.github.com>
2024-09-17 12:30:49 +0700

    Update SoDoHoiNghi.js

--- src/SoDoHoiNghi.js
+++ src/SoDoHoiNghi.js
@@ -7,7 +7,7 @@ const SoDoHoiNghi = () => {
         <div className="chaomung-wrapper position-relative" style={{ backgroundSize: 'cover', backgroundPosition: 'top' }}>
             <div className="h-100 d-flex align-items-center">
                 <div className="w-100 d-flex justify-content-center align-items-center">
-                    <div className="user-info text-center mb-4" style={{ width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <div className="user-info text-center mb-4" style={{ width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', overflowY: 'auto', maxHeight: '80vh' }}>
                         <img src={titlehoinghi} style={{ width: "90%", marginTop: "1rem" }} alt="hoinghi Title" />
                         <img src={sodohoinghi} style={{ width: "70%", marginTop: "1rem" }} alt="hoinghi Image" />
                     </div>
